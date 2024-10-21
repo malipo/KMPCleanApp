21e7f57ba2b8124b67f71c5bdda59f2bf573cc65
Malihe <malihe.pooraskari@gmail.com>
2024-10-21 17:37:54 +0330

    clean koin

--- kmpCore/network/src/commonMain/kotlin/com/jetbrains/network/api/Network.kt
+++ kmpCore/network/src/commonMain/kotlin/com/jetbrains/network/api/Network.kt
@@ -1,40 +1,16 @@
 package com.jetbrains.network.api
 
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.SIMPLE
 import io.ktor.http.ContentType
-import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
-//ph
-fun createHttpClient(engine: HttpClientEngine): HttpClient {
-    return HttpClient(engine) {
-        install(Logging) {
-            level = LogLevel.ALL
-        }
-        install(ContentNegotiation) {
-            json(
-                json = Json {
-                    ignoreUnknownKeys = true
-                    prettyPrint = true
-                    isLenient = true
-                    explicitNulls = false
-                    coerceInputValues = true
-                }
-            )
-        }
-    }
-}
-
-//h--
-object Network{
+class Network {
     val httpClient = HttpClient {
         install(ContentNegotiation) {
             json(Json {
@@ -43,7 +19,7 @@ object Network{
                 isLenient = true
                 explicitNulls = false
                 coerceInputValues = true
-            })
+            }, contentType = ContentType.Any)
         }
         install(Logging) {
             logger = Logger.SIMPLE
@@ -51,11 +27,6 @@ object Network{
         }
 
         expectSuccess = true
-
-        defaultRequest {
-            url(APIs.GET_MUSEUM)
-            contentType(ContentType.Application.Json)
-        }
 
     }
 }
